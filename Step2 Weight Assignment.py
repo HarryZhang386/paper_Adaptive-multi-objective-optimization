09eef7c13d039ee4f9736099c4d84c0a529726a8
HarryZhangnus <xudong.zhang@u.nus.edu>
2026-01-01 17:07:38 +0800

    Step2 Weight Assignment.py

--- Step2 Weight Assignment.py
+++ Step2 Weight Assignment.py
@@ -10,7 +10,7 @@ You need to:
 3. Tune the steepness parameter for weight function
 4. Apply weights to select optimization solutions
 
-Repository: [https://github.com/HarryZhang386/paper_Adaptive-multi-objective-optimization.git]
+Repository: [https://github.com/HarryZhangnus/paper_Adaptive-multi-objective-optimization.git]
 """
 
 import numpy as np
